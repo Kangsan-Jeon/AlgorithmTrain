392553ef2f3e8278dbd3c1580a0424c77ee9d32c
JKS <tks0204@naver.com>
2019-10-13 13:57:42 +0900

    13460번 문제 풀이 코드 추가

--- Baekjoon/[완]13460_구슬 탈출2.py
+++ Baekjoon/[완]13460_구슬 탈출2.py
@@ -1,12 +1,9 @@
-# 21:37
-
 from collections import deque
-from pprint import pprint
 
 dx = [-1, 1, 0, 0]  # left, right, up, down
 dy = [0, 0, -1, 1]
 
-def go(myMap, table, visited, blue, red, d_y, d_x, myQ):
+def go(myMap, visited, blue, red, d_y, d_x, cnt, myQ):
     # print("origin: red = {}, blue = {}".format(red, blue))
     red_y, red_x = red
     blue_y, blue_x = blue
@@ -23,10 +20,9 @@ def go(myMap, table, visited, blue, red, d_y, d_x, myQ):
         red_x += d_x
         blue_y += d_y
         blue_x += d_x
-        table[red_y][red_x] = table[red[0]][red[1]] + 1
 
     if blue_state:
-        return False
+        return 0
 
     if myMap[red_y][red_x] == "#" or red_state:
         while(myMap[blue_y][blue_x] != "#"):
@@ -41,7 +37,6 @@ def go(myMap, table, visited, blue, red, d_y, d_x, myQ):
                 break
     else:
         while(myMap[red_y][red_x] != "#"):
-            table[red_y][red_x] = table[red[0]][red[1]] + 1
             if myMap[red_y][red_x] == "O":
                 red_state = True
                 break
@@ -53,35 +48,37 @@ def go(myMap, table, visited, blue, red, d_y, d_x, myQ):
                 break
 
     if blue_state:
-        return False
+        return 0
     elif red_state:
-        return True
+        return cnt+1
     else:
         temp = [(blue_y - d_y, blue_x - d_x), (red_y - d_y, red_x - d_x)]
-        if temp[1] == red:
-            table[red[0]][red[1]] += 1
-        if temp not in visited:
+        if (temp[0], temp[1]) not in visited:
+            visited.append((temp[0], temp[1]))
+            temp += [cnt+1]
             myQ.append(temp)
-        return False
+        return 0
 
-def solve(myMap, table, visited, blue, red, hole):
+def solve(myMap, blue, red):
     myQ = deque()
-    myQ.append([blue, red])
+    myQ.append([blue, red, 0])
+    visited = [(blue, red)]
     while (len(myQ) != 0):
-        print(myQ)
         temp = myQ.popleft()
         blue_y, blue_x = temp[0]
         red_y, red_x = temp[1]
-        visited.append((temp))
+        cnt = temp[2]
         for i in range(4):
             new_red_y = red_y + dy[i]
             new_red_x = red_x + dx[i]
             new_blue_y = blue_y + dy[i]
             new_blue_x = blue_x + dx[i]
-            if ([(new_blue_y, new_blue_x), (new_red_y, new_red_x)] not in visited) and (myMap[new_red_y][new_red_x] != "#" or myMap[new_blue_y][new_blue_x] != "#"):
-                flag = go(myMap, table, visited, (blue_y, blue_x), (red_y, red_x), dy[i], dx[i], myQ)
+            if (myMap[new_red_y][new_red_x] != "#" or myMap[new_blue_y][new_blue_x] != "#"):
+                flag = go(myMap, visited, (blue_y, blue_x), (red_y, red_x), dy[i], dx[i], cnt, myQ)
                 if flag:
-                    return table[hole[0]][hole[1]]
+                    if flag > 10:
+                        flag = -1
+                    return flag
                 else:
                     continue
 
@@ -98,14 +95,10 @@ def main():
                 blue = (i, j)
             elif temp[j] == "R":
                 red = (i, j)
-            elif temp[j] == "O":
-                hole = (i, j)
 
         myMap.append(temp)
 
-    table = [[0 for _ in range(M)] for _ in range(N)]
-    visited = []
-    result = solve(myMap, table, visited, blue, red, hole)
+    result = solve(myMap, blue, red)
     print(result)
 
 
